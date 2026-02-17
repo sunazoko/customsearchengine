30c04ff015b15c9a3a95be18245d4f10b99e98f1
sunazoko <mustangsub@yahoo.co.jp>
2026-02-18 02:15:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const params = new URLSearchParams(window.location.search);
 const query = params.get("q");
 
-const output = document.getElementById("output");
-
 if (query) {
   const decoded = decodeURIComponent(query);
-  output.textContent = decoded;
-} else {
-  output.textContent = "No query";
+
+  const target =
+    "https://duckduckgo.com/?q=" + encodeURIComponent(decoded);
+
+  window.location.replace(target);
 }
